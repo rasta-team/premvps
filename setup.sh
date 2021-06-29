1c927df9411be47641f772af8568887282547b97
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-06-30 00:52:24 +0800

    Update setup.sh
    disable history clear

--- setup.sh
+++ setup.sh
@@ -90,7 +90,7 @@ systemctl daemon-reload
 systemctl enable autosett
 #wget -O /etc/set.sh "https://raw.githubusercontent.com/gilagajet/premvps/main/set.sh"
 #chmod +x /etc/set.sh
-history -c
+#history -c
 echo "1.2" > /home/ver
 clear
 echo " "
