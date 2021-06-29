ba62985b7ed83c1e74c1ee9bc3c13e79fae92064
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-06-30 00:22:22 +0800

    Update setup.sh
    disabled set.sh
    #file not found

--- setup.sh
+++ setup.sh
@@ -88,8 +88,8 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/gilagajet/premvps/main/set.sh"
-chmod +x /etc/set.sh
+#wget -O /etc/set.sh "https://raw.githubusercontent.com/gilagajet/premvps/main/set.sh"
+#chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
 clear
