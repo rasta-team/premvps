ce8e7da0456bbbbdbfc19eefb94dab7866d769f2
gilagajet <84836081+gilagajet@users.noreply.github.com>
2021-06-30 03:23:22 +0800

    Update autobackup.sh

--- autobackup.sh
+++ autobackup.sh
@@ -40,7 +40,7 @@ sleep 1
 echo " Please Wait"
 clear
 echo " Autobackup Has Been Started"
-echo " Data Will Be Backed Up Automatically at 00:05 GMT +7"
+echo " Data Will Be Backed Up Automatically at 00:05 GMT +8"
 exit 0
 }
 function stop() {
